80fed71f8f6bdbc8ca33fb4a595ec8814d2bc500
Luis Escobar <lescoba1@nd.edu>
2015-04-21 04:34:44 -0400

    Initial Commit

---
+++ AlertMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlertMap
+//
+//  Created by Lucho Escobar on 4/21/15.
+//  Copyright (c) 2015 Lucho & Antonio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
